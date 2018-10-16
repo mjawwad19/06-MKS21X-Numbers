5a07aef0c0c4ae1ce90f0ec08e81bb9d6eb158ec
Mohammad Jawwad <mjawwad@stuy.edu>
2018-10-16 10:57:30 -0400

    Updated RealNumber class with Mr.K's given empty method class

--- RealNumber.java
+++ RealNumber.java
@@ -1,22 +1,43 @@
 public class RealNumber{
-  private double x;
+  private double value;
 
-  public RealNumber(double val) {
-    x = val;
+  public RealNumber(double v){
+    value = v;
   }
 
-  public RealNumber() {
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+    return 0;
   }
-
-  public double getVal() {
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return 0;
+  }
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return 0;
+  }
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
     return 0;
   }
 
-  public String toString() {
-    return "";
+
+  public double getValue(){
+    return value;
   }
 
-  public boolean compareTo(RealNumber a) {
-    return false;
+  public String toString(){
+    return ""+value;
   }
 }
+
+
